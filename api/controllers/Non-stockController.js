b9a2ef911a352c75e0664c3d0decee22a246e13e
Bathoin <jhnguyen@unomaha.edu>
2017-02-01 13:30:00 -0600

    Customer/Non-stock/stock controllers

---
+++ api/controllers/Non-stockController.js
@@ -0,0 +1,21 @@
+/**
+ * Non-stockController
+ *
+ * @description :: Server-side logic for managing non-stocks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `Non-stockController.Investments()`
+   */
+  Investments: function (req, res) {
+    return res.json({
+      todo: 'Investments() is not implemented yet!'
+    });
+  }
+};
+
